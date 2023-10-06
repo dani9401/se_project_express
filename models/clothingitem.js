e1d6149eb9bb4d365c74c01b18ee730cea1aca64
Danielle Foss <danifoss2011@gmail.com>
2023-10-05 19:42:27 -0600

    add clothingItem and user schemas

---
+++ models/clothingitem.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const clothingItem = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+
+  },
+  weather: {
+    type: String,
+    required: true,
+    enum: ["hot", "warm", "cold"],
+  },
+  imageURL: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: "Link is not valid",
+    }
+  },
+});
+
+module.exports = mongoose.model("clothingItems", clothingItem);
