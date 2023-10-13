d7b5450f11a316a571134b33856b1a78a3bf8b37
Danielle Foss <danifoss2011@gmail.com>
2023-10-13 09:57:46 -0600

    fixed error code

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const clothingItemRoutes = require("./clothingItem");
 const userRoutes = require("./user");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/items", clothingItemRoutes);
 router.use("/users", userRoutes);
 
 router.use((req, res) => {
-  res.status(500).send({ message: "Router not found" });
+  res.status(NOT_FOUND).send({
+    message: "The request was sent to a non-existent address",
+  });
 });
 
 module.exports = router;
