ee6c37af031bf8e66dd4b89a927761ced0469bd8
Danielle Foss <danifoss2011@gmail.com>
2023-10-17 13:46:20 -0600

    update findUserByCredentials

--- models/user.js
+++ models/user.js
@@ -30,6 +30,7 @@ const user = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 8,
+    select: false,
   },
 });
 
@@ -37,19 +38,21 @@ user.statics.findUserByCredentials = function findUserByCredentials(
   email,
   password,
 ) {
-  return this.findOne({ email }).then((user) => {
-    if (!user) {
-      return Promise.reject(new Error("Incorrect email or password"));
-    }
-
-    return bcrypt.compare(password, user.password).then((matched) => {
-      if (!matched) {
+  return this.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
         return Promise.reject(new Error("Incorrect email or password"));
       }
 
-      return user;
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return Promise.reject(new Error("Incorrect email or password"));
+        }
+
+        return user;
+      });
     });
-  });
 };
 
 module.exports = mongoose.model("users", user);
