8c823c837da2772bf4e70ccd9f2188984bf406e2
Danielle Foss <danifoss2011@gmail.com>
2023-10-17 12:54:43 -0600

    add 401 error

--- utils/errors.js
+++ utils/errors.js
@@ -1,9 +1,11 @@
 const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
 const NOT_FOUND = 404;
 const DEFAULT_ERROR = 500;
 
 module.exports = {
   BAD_REQUEST,
+  UNAUTHORIZED,
   NOT_FOUND,
   DEFAULT_ERROR,
 };
