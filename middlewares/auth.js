c0f23d8d1a8615cb84c8cfa6e9be050d980ba3fd
Danielle Foss <danifoss2011@gmail.com>
2023-10-18 20:35:50 -0600

    clean up extra code notes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,9 +2,10 @@ const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const { UNAUTHORIZED } = require("../utils/errors");
 
-const handleAuthError = (res, err) => {
-  console.error(err);
-  res.status(UNAUTHORIZED).send({ message: "Authorization Error" });
+const handleAuthError = (res) => {
+  res
+    .status(UNAUTHORIZED)
+    .send({ message: "Authorization Error from handleAuthError" });
 };
 
 module.exports = (req, res, next) => {
@@ -13,7 +14,7 @@ module.exports = (req, res, next) => {
 
   // let's check the header exists and starts with 'Bearer '
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    return handleAuthError(res, err);
+    return handleAuthError(res);
   }
   // getting the token
   const token = authorization.replace("Bearer ", "");
@@ -24,7 +25,7 @@ module.exports = (req, res, next) => {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     console.error(err);
-    return handleAuthError(res, err);
+    return handleAuthError(res);
   }
 
   req.user = payload; // adding the payload to the Request object
