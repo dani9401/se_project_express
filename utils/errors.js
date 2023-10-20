7525cc986a75ca8dee031e277c03318435c0e001
Danielle Foss <danifoss2011@gmail.com>
2023-10-20 15:34:30 -0600

    add duplicate error

--- utils/errors.js
+++ utils/errors.js
@@ -2,6 +2,7 @@ const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
 const FORBIDDEN = 403;
 const NOT_FOUND = 404;
+const DUPLICATE = 409;
 const DEFAULT_ERROR = 500;
 
 module.exports = {
@@ -10,4 +11,5 @@ module.exports = {
   FORBIDDEN,
   NOT_FOUND,
   DEFAULT_ERROR,
+  DUPLICATE,
 };
