444ae8c053ba3bcfb73308776f0cae95911b9e57
Danielle Foss <danifoss2011@gmail.com>
2023-10-20 15:34:05 -0600

    refactored createUser and loginUser

--- controllers/user.js
+++ controllers/user.js
@@ -2,39 +2,47 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const User = require("../models/user");
-const { BAD_REQUEST, UNAUTHORIZED, DEFAULT_ERROR } = require("../utils/errors");
+const {
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  DEFAULT_ERROR,
+  DUPLICATE,
+} = require("../utils/errors");
 const opts = { runValidators: true };
 
 const createUser = (req, res) => {
   const { name, avatar, email, password } = req.body;
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash) => User.create({ name, avatar, email, password: hash }))
-    .then((newUser) => {
-      res.send({ data: newUser });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        res.status(BAD_REQUEST).send({
-          message: "This field accepts a value between 2 and 30 characters",
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      return res
+        .status(DUPLICATE)
+        .send({ message: "Email already exists in system" });
+    } else
+      return bcrypt
+        .hash(password, 10)
+        .then((hash) => User.create({ name, avatar, email, password: hash }))
+        .then((newUser) => {
+          res.send({ data: newUser });
+        })
+        .catch((err) => {
+          console.error(err);
+          if (err.name === "ValidationError") {
+            res.status(BAD_REQUEST).send({
+              message: err.message,
+            });
+          } else if (error.code === 11000) {
+            console.error("Duplicate key error. Document already exists!");
+            res.status(DUPLICATE).send({
+              message: "Email already exists in our system.",
+            });
+          } else {
+            res
+              .status(DEFAULT_ERROR)
+              .send({ message: "An error has occurred on the server." });
+          }
         });
-      } else if (err.name === "CastError") {
-        res.status(BAD_REQUEST).send({
-          message: "New undefined Cast Error.",
-        });
-      } else if (error.code === 11000) {
-        console.error("Duplicate key error. Document already exists!");
-        res.status(11000).send({
-          message: "Error from createUser.",
-        });
-      } else {
-        res
-          .status(DEFAULT_ERROR)
-          .send({ message: "An error has occurred on the server." });
-      }
-    });
+  });
 };
 
 const loginUser = (req, res) => {
@@ -50,8 +58,8 @@ const loginUser = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        res.status(UNAUTHORIZED).send({
-          message: "Incorrect email or password",
+        res.status(BAD_REQUEST).send({
+          message: "Incorrect email address or password.",
         });
       } else {
         res
