6dd2da95e514d3bc630164c732d97d5858d068ef
Danielle Foss <danifoss2011@gmail.com>
2023-10-19 22:01:38 -0600

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const { PORT = 3001 } = process.env;
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/wtwr_db",
