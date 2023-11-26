a840cde4423131456b8e7135a1889c8bcbe374c2
Danielle Foss <danifoss2011@gmail.com>
2023-11-26 11:09:07 -0700

    create errorHandling middleware file and import to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandling");
 
 const { PORT = 3001 } = process.env;
 const app = express();
@@ -19,6 +20,11 @@ const routes = require("./routes");
 
 app.use(express.json());
 app.use(routes);
+app.use(errorHandler);
+//app.use((err, req, res, next) => {
+//  console.error(err);
+//  return res.status(500).send({ message: "An error occurred on the server" });
+//});
 
 app.listen(PORT, () => {
   console.log(`App listening at port ${PORT}`);
