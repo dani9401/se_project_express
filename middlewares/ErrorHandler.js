4e844cb44afc3e340fc6685fd13461eab20163d3
Danielle Foss <danifoss2011@gmail.com>
2023-12-03 12:15:40 -0700

    update errorHandler

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -1,20 +1,11 @@
-class ErrorHandler {
-  constructor(message) {
-    (this.message = message), (this.statusCode = 500);
-  }
-}
-//app.use((err, req, res, next) => {
-//  console.error(err);
-//  // ***if an error has no status, set it to 500***
-//  const { statusCode = 500, message } = err;
-//  res
-//    .status(statusCode)
-//    .send({
-//      // **check the status and display a message based on it**
-//      message: statusCode === 500
-//        ? 'An error occurred on the server'
-//        : message
-//    });
-//});
+const errorHandler = (err, req, res, next) => {
+  console.error(err);
+  // ***if an error has no status, set it to 500***
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    // **check the status and display a message based on it**
+    message: statusCode === 500 ? "An error occurred on the server" : message,
+  });
+};
 
-module.exports = ErrorHandler;
+module.exports = errorHandler;
