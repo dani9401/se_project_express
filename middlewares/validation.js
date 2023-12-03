0c2b03e86896d8c8442908fbf97f1baabb0522e1
Danielle Foss <danifoss2011@gmail.com>
2023-12-03 15:17:29 -0700

    add validation for URLs

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -13,13 +13,28 @@ const {
   updateUser,
 } = require("../controllers/user");
 
+// - - - URL Validator Function - - -
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
 // - - - - createItem - - - -
 router.post(
   "/posts",
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      imageUrl: Joi.string().required().min(2), //how to add URL format?
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
+      imageUrl: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "imageUrl" field must be filled in',
+        "string.uri": 'the "imageUrl" field must be a valid url',
+      }),
     }),
   }),
   createItem,
@@ -30,10 +45,23 @@ router.post(
   "/posts",
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      avatar: Joi.string().required().min(2), //how to add URL format?
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
+      avatar: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "avatar" field must be filled in',
+        "string.uri": 'the "avatar" field must be a valid url',
+      }),
+      email: Joi.string().required().email().messages({
+        "string.empty": 'The "email" field must be filled in',
+        "string.uri": 'the "email" field must be a valid email',
+      }),
+      password: Joi.string().required().min(8).messages({
+        "string.empty": 'The "password" field must be filled in',
+        "string.min": 'The minimum length of the "password" field is 8',
+      }),
     }),
   }),
   createUser,
@@ -44,8 +72,14 @@ router.post(
   "/posts",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      email: Joi.string().required().email().messages({
+        "string.empty": 'The "email" field must be filled in',
+        "string.uri": 'the "email" field must be a valid email',
+      }),
+      password: Joi.string().required().min(8).messages({
+        "string.empty": 'The "password" field must be filled in',
+        "string.min": 'The minimum length of the "password" field is 8',
+      }),
     }),
   }),
   loginUser,
@@ -101,6 +135,17 @@ router.patch(
   celebrate({
     params: Joi.object().keys({
       userId: Joi.string().hexadecimal().length(24), //check hexadecimal setup correctly
+    }),
+    body: Joi.object().keys({
+      name: Joi.string().required().min(2).max(30).messages({
+        "string.min": 'The minimum length of the "name" field is 2',
+        "string.max": 'The maximum length of the "name" field is 30',
+        "string.empty": 'The "name" field must be filled in',
+      }),
+      avatar: Joi.string().required().custom(validateURL).messages({
+        "string.empty": 'The "avatar" field must be filled in',
+        "string.uri": 'the "avatar" field must be a valid url',
+      }),
     }),
   }),
   updateUser,
