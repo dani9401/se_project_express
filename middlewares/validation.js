c71aa2566b6acdc08387220db6fcf7869e91bc9d
Danielle Foss <danifoss2011@gmail.com>
2023-12-22 13:34:39 -0700

    fix validation error on createItem

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -35,37 +35,37 @@ router.post(
         "string.empty": 'The "imageUrl" field must be filled in',
         "string.uri": 'the "imageUrl" field must be a valid url',
       }),
+      weather: Joi.string().valid("hot", "warm", "cold").required(),
     }),
-  }).unknown(true),
+  }),
   createItem,
-);
-
-// - - - - createUser - - - -
-router.post(
-  "/posts",
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30).messages({
-        "string.min": 'The minimum length of the "name" field is 2',
-        "string.max": 'The maximum length of the "name" field is 30',
-        "string.empty": 'The "name" field must be filled in',
+),
+  // - - - - createUser - - - -
+  router.post(
+    "/posts",
+    celebrate({
+      body: Joi.object().keys({
+        name: Joi.string().required().min(2).max(30).messages({
+          "string.min": 'The minimum length of the "name" field is 2',
+          "string.max": 'The maximum length of the "name" field is 30',
+          "string.empty": 'The "name" field must be filled in',
+        }),
+        avatar: Joi.string().required().custom(validateURL).messages({
+          "string.empty": 'The "avatar" field must be filled in',
+          "string.uri": 'the "avatar" field must be a valid url',
+        }),
+        email: Joi.string().required().email().messages({
+          "string.empty": 'The "email" field must be filled in',
+          "string.uri": 'the "email" field must be a valid email',
+        }),
+        password: Joi.string().required().min(8).messages({
+          "string.empty": 'The "password" field must be filled in',
+          "string.min": 'The minimum length of the "password" field is 8',
+        }),
       }),
-      avatar: Joi.string().required().custom(validateURL).messages({
-        "string.empty": 'The "avatar" field must be filled in',
-        "string.uri": 'the "avatar" field must be a valid url',
-      }),
-      email: Joi.string().required().email().messages({
-        "string.empty": 'The "email" field must be filled in',
-        "string.uri": 'the "email" field must be a valid email',
-      }),
-      password: Joi.string().required().min(8).messages({
-        "string.empty": 'The "password" field must be filled in',
-        "string.min": 'The minimum length of the "password" field is 8',
-      }),
-    }),
-  }).unknown(true),
-  createUser,
-);
+    }).unknown(true),
+    createUser,
+  );
 
 // - - - - loginUser - - - -
 router.post(
